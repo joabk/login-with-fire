e13fa3367f1771904bb74e9f46c50eb3960c608a
joabk <mjoab88@gmail.com>
2020-01-31 17:06:52 +0300

    3 files modified, 5 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterModule,  } from '@angular/router';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+
+  name = 'Angular';  
+  constructor(private router: RouterModule){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'login', component: LoginComponent}
+    ])
+    ],
+  declarations: [ AppComponent, HelloComponent, LoginComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
